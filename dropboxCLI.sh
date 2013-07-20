25732c4f89b52462e775ab21130036d53baf694c
mrjjj <jonathan.romiguier@gmail.com>
2013-07-20 09:56:07 +0200

    Add common exclusion list

--- dropboxCLI.sh
+++ dropboxCLI.sh
@@ -4,5 +4,9 @@
 #64-bit:
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
-#Ensuite, lancez le démon Dropbox à partir du nouveau fichier .dropbox-dist.
-~/.dropbox-dist/dropboxd
+wget -O ~/bin/dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
+chmod 777 ~/bin/dropbox.py
+
+~/.dropbox-dist/dropboxd &
+
+#~/bin/dropbox.py exclude add ~/Dropbox/Android ~/Dropbox/Camera\ Uploads ~/Dropbox/Chrome ~/Dropbox/ebooks ~/Dropbox/evernote ~/Dropbox/Images ~/Dropbox/Mangas ~/Dropbox/Musique ~/Dropbox/Notes ~/Dropbox/Papiers ~/Dropbox/Photos ~/Dropbox/ppt ~/Dropbox/pss ~/Dropbox/Public
